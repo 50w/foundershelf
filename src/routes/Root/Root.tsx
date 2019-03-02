4061854c0dc54667800647660dc21605992b018d
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-03-02 14:16:27 -0500

    Add grid tiles

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -1,7 +1,73 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery';
+
+// TileList -------------------------------------
+function AdvancedGridList(props:any) {
+    const { classes } = props;
+    const matches = useMediaQuery('(min-width:768px)');
+
+    return (
+      <div className={classes.root}>
+        <GridList cellHeight={200} spacing={1} cols={matches ? 3 : 2} className={classes.gridList}>
+          {shelf.people.map(person => (
+            <GridListTile key={person.img} cols={1} rows={2}>
+              <img src={person.img} alt={person.name} />
+              <GridListTileBar
+                title={person.name}
+                titlePosition="top"
+                actionIcon={
+                  <IconButton className={classes.icon}>
+                    <StarBorderIcon />
+                  </IconButton>
+                }
+                actionPosition="left"
+                className={classes.titleBar}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    );
+  }
+
+  AdvancedGridList.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+const MyTileList = withStyles({
+    root: {
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      width: '100%',
+      maxHeight: 'calc(100VH-20rem)',
+      paddingBottom: '1rem',
+    },
+    gridList: {
+      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+      transform: 'translateZ(0)',
+      boxShadow: '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)',
+
+    },
+    titleBar: {
+      background:
+        'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+        'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+    icon: {
+      color: 'white',
+    },
+})(AdvancedGridList);
 
 import shelf from '../../shelf';
-
+import './Root.css'
 class Root extends Component {
   render() {
     console.log(shelf)
