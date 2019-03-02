36ac8cbbe2d70c91e9f6903cf8ab3702f507fdf8
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-03-02 15:35:45 -0500

    Updates

---
+++ precache-manifest.b12b39b860035cb5d2d3a97280e08475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "274cdf70b5359eb47648",
+    "url": "/static/js/main.274cdf70.chunk.js"
+  },
+  {
+    "revision": "3dd762eb50e4b14d3273",
+    "url": "/static/js/2.3dd762eb.chunk.js"
+  },
+  {
+    "revision": "274cdf70b5359eb47648",
+    "url": "/static/css/main.3aa615dd.chunk.css"
+  },
+  {
+    "revision": "6f8dd17c2194cb16e3c0ce90e420ecbb",
+    "url": "/index.html"
+  }
+];
