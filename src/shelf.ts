f08c8fdd44c26568b9d72874a043455db22b1d09
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-03-02 12:23:55 -0500

    Functions to convert book/person names to book/person object

--- src/shelf.ts
+++ src/shelf.ts
@@ -28,3 +28,19 @@ interface Shelf {
     people: Person[]
 }
 
+// Utilities ------------------------------------
+function titlesToBooks(arr: string[]):Book[] {
+    const books: Book[] = []
+    arr.forEach(element => {
+        library[element] ? books.push(library[element]) : null;
+    });
+    return books
+}
+
+function namesToPeople(arr: string[]):Person[] {
+    const people: Person[] = []
+    arr.forEach(element => {
+        persons[element] ? people.push(persons[element]) : null;
+    });
+    return people
+}
