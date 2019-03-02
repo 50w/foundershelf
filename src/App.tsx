6bd0893006e85c5a62d3b102f80bc7424373dcc9
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-03-02 18:04:10 -0500

    Add Card header to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Card from '@material-ui/core/Card';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import {Root} from './routes';
@@ -12,6 +13,14 @@ class App extends Component {
       <>
       {/* <AppBar/> */}
       <div className="App">
+      <Card raised style={{margin: '1rem 0.25rem 1rem 0.25rem', padding: '0.5rem 1rem 0.5rem 1rem', textAlign: 'left'}}>
+          <div>
+              <h2>What is Founder Shelfspace?</h2>
+              <p>
+                What does the bookshelf of the worlds most successful founders, entrepreneurs, and investors look like? Get context on how they think and browse the growing library of books they recommend...
+              </p>
+          </div>
+        </Card>
         <Router>
           <Route path='/' component={Root}/>
         </Router>
