f3c04c7336493adc9082f3e491bb11946a0b6930
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-03-02 12:24:35 -0500

    Init 'library' with some tobi recommendations 🛍

--- src/shelf.ts
+++ src/shelf.ts
@@ -44,3 +44,22 @@ function namesToPeople(arr: string[]):Person[] {
     });
     return people
 }
+
+// Data -----------------------------------------
+const library:Books = {
+    'Mindset': {
+        title: 'Mindset',
+        author: 'Carol Dweck',
+        img: 'https://images.gr-assets.com/books/1364251156l/1307582.jpg'
+    },
+    'Thinking in Systems': {
+        title: 'Thinking in Systems',
+        author: 'Donella Meadows',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/61npQHfWgAL.jpg',
+    },
+    'Why We Sleep: Unlocking the Power of Sleep and Dreams': {
+        title: 'Why We Sleep: Unlocking the Power of Sleep and Dreams',
+        author: 'Matthew Walker',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/81w6RZ6xm1L.jpg'
+    }
+}
