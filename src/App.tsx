6d0aef5091025481fa6fea7eab2f205496448d9d
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-03-02 14:18:02 -0500

    Add AppBar to homescreen

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import {Root} from './routes';
+import{AppBar} from './components'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <>
+      <AppBar/>
       <div className="App">
         <Router>
           <Route path='/' component={Root}/>
         </Router>
       </div>
+      </>
     );
   }
 }
