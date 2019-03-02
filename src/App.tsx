00b0d24a98fdf218c6f254d93d5c8d0825729662
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-03-02 15:29:46 -0500

    Remove AppBar for now

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <>
-      <AppBar/>
+      {/* <AppBar/> */}
       <div className="App">
         <Router>
           <Route path='/' component={Root}/>
