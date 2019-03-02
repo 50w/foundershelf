499049a3d4b017adb293e0a859e4d7fd9db3da3f
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-03-02 15:07:16 -0500

    Big Jeff added

--- src/shelf.ts
+++ src/shelf.ts
@@ -87,12 +87,42 @@ const library:Books = {
         author: "Peter Thiel",
         img: "",
     },
+    "The Remains of the Day": {
+        title: "The Remains of the Day",
+        author: "Kazuo Ishiguro",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/71d9G7xwaLL.jpg",
     },
-    'Why We Sleep: Unlocking the Power of Sleep and Dreams': {
-        title: 'Why We Sleep: Unlocking the Power of Sleep and Dreams',
-        author: 'Matthew Walker',
-        img: 'https://images-na.ssl-images-amazon.com/images/I/81w6RZ6xm1L.jpg'
+    "Built to Last: Successful Habits of Visionary Companies": {
+        title: "Built to Last: Successful Habits of Visionary Companies",
+        author: "Jim Collins",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/412OMhMA+lL._SX330_BO1,204,203,200_.jpg",
+    },
+    "Creation: Life and How to Make It": {
+        title: "Creation: Life and How to Make It",
+        author: "Steve Grand",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/41Y5MRAJP9L._SX328_BO1,204,203,200_.jpg"
+    },
+    "Good to Great: Why Some Companies Make the Leap … and Others Don't": {
+        title: "Good to Great: Why Some Companies Make the Leap … and Others Don't",
+        author: "Jim Collins",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/41VrxaCVU2L.jpg"
+    },
+    "The Innovator's Dilemma": {
+        title: "The Innovator's Dilemma",
+        author: "Clayton Christensen",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2016/09/28/TB-3_The_Innovators_Dilemma.jpg",
+    },
+    "Sam Walton: Made in America": {
+        title: "Sam Walton: Made in America",
+        author: "Sam Walton",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/51hdHY1gTSL._SX299_BO1,204,203,200_.jpg"
+    },
+    "The Mythical Man-Month": {
+        title: "The Mythical Man-Month",
+        author: "Frederick P. Brooks, Jr.",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/51XnDL5KC+L._SX334_BO1,204,203,200_.jpg"
     }
+
 }
 
 const persons:People = {
@@ -109,6 +139,13 @@ const persons:People = {
         description: "South African entrepreneur Elon Musk is known for founding Tesla Motors and SpaceX, which launched a landmark commercial spacecraft in 2012.",
         company: "Tesla Motors & SpaceX",
         books: titlesToBooks(["The Hitchhiker's Guide to the Galaxy", "Einstein: His Life and Universe", "Structures: Or Why Things Don't Fall Down", "Superintelligence: Paths, Dangers, Strategies", "Zero to One: Notes on Startups, or How to Build the Future",]), 
+    },
+    "Jeff Bezos" : {
+        name: "Jeff Bezos",
+        img: "https://specials-images.forbesimg.com/imageserve/5b8eb1b3a7ea434b99d54b36/960x0.jpg?fit=scale",
+        description: "American entrepreneur Jeff Bezos is the founder and chief executive officer of Amazon.com and owner of 'The Washington Post.' His successful business ventures have made him one of the richest people in the world.",
+        company: "Amazon & The Washington Post",
+        books: titlesToBooks(["The Remains of the Day", "Built to Last: Successful Habits of Visionary Companies", "Creation: Life and How to Make It", "Good to Great: Why Some Companies Make the Leap … and Others Don't", "The Innovator's Dilemma", "Sam Walton: Made in America", "The Mythical Man-Month"]), 
     },
     }
 }
