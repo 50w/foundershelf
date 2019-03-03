c82fc00a6f4860f26173016820064565e92cc5c1
Connor Maloney <foopert@gmail.com>
2019-03-03 13:33:08 -0500

    ADD: extracted books into own file

---
+++ src/books.ts
@@ -0,0 +1,91 @@
+import {Books} from './types'
+
+const books:Books = {
+    "Mindset": {
+        title: "Mindset",
+        author: "Carol Dweck",
+        img: "https://images.gr-assets.com/books/1364251156l/1307582.jpg"
+    },
+    "Thinking in Systems": {
+        title: "Thinking in Systems",
+        author: "Donella Meadows",
+        img: "https://images-na.ssl-images-amazon.com/images/I/61npQHfWgAL.jpg",
+    },
+    "Why We Sleep: Unlocking the Power of Sleep and Dreams": {
+        title: "Why We Sleep: Unlocking the Power of Sleep and Dreams",
+        author: "Matthew Walker",
+        img: "https://images-na.ssl-images-amazon.com/images/I/81w6RZ6xm1L.jpg"
+    },
+    "The Hitchhiker's Guide to the Galaxy": {
+        title: "The Hitchhiker's Guide to the Galaxy",
+        author: "Douglas Adams",
+        img: "",
+    },
+    "Einstein: His Life and Universe": {
+        title: "Einstein: His Life and Universe",
+        author: "Walter Isaacson",
+        img: ""
+    },
+    "Structures: Or Why Things Don't Fall Down": {
+        title: "Structures: Or Why Things Don't Fall Down",
+        author: "J.E. Gordon",
+        img: "",
+    },
+    "Superintelligence: Paths, Dangers, Strategies": {
+        title: "Superintelligence: Paths, Dangers, Strategies",
+        author: "Nick Bostrom",
+        img: "",
+    },
+    "Zero to One: Notes on Startups, or How to Build the Future": {
+        title: "Zero to One: Notes on Startups, or How to Build the Future",
+        author: "Peter Thiel",
+        img: "",
+    },
+    "The Remains of the Day": {
+        title: "The Remains of the Day",
+        author: "Kazuo Ishiguro",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/71d9G7xwaLL.jpg",
+    },
+    "Built to Last: Successful Habits of Visionary Companies": {
+        title: "Built to Last: Successful Habits of Visionary Companies",
+        author: "Jim Collins",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/412OMhMA+lL._SX330_BO1,204,203,200_.jpg",
+    },
+    "Creation: Life and How to Make It": {
+        title: "Creation: Life and How to Make It",
+        author: "Steve Grand",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/41Y5MRAJP9L._SX328_BO1,204,203,200_.jpg"
+    },
+    "Good to Great: Why Some Companies Make the Leap … and Others Don't": {
+        title: "Good to Great: Why Some Companies Make the Leap … and Others Don't",
+        author: "Jim Collins",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/41VrxaCVU2L.jpg"
+    },
+    "The Innovator's Dilemma": {
+        title: "The Innovator's Dilemma",
+        author: "Clayton Christensen",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2016/09/28/TB-3_The_Innovators_Dilemma.jpg",
+    },
+    "Sam Walton: Made in America": {
+        title: "Sam Walton: Made in America",
+        author: "Sam Walton",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/51hdHY1gTSL._SX299_BO1,204,203,200_.jpg"
+    },
+    "The Mythical Man-Month": {
+        title: "The Mythical Man-Month",
+        author: "Frederick P. Brooks, Jr.",
+        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/51XnDL5KC+L._SX334_BO1,204,203,200_.jpg"
+    },
+    "Buffett: The Making of an American Capitalist": {
+        title: "Buffett: The Making of an American Capitalist",
+        author: "Roger Lowenstein",
+        img: "https://www.theceolibrary.com/wp-content/uploads/2019/02/Buffett-The-Making-of-an-American-Capitalist-Roger-Lowenstein.jpeg"
+    },
+    "Blitzscaling: The Lightning-Fast Path to Building Massively Valuable Companies": {
+        title: "Blitzscaling: The Lightning-Fast Path to Building Massively Valuable Companies",
+        author: "Reid Hoffman, Chris Yeh",
+        img: "https://www.theceolibrary.com/wp-content/uploads/2019/01/Blitzscaling-Reid-Hoffman-Chris-Yeh.jpg"    
+    }
+}
+
+export default books
--- src/shelf.ts
+++ src/shelf.ts
@@ -1,6 +1,5 @@
-import { string } from "prop-types";
 import {Shelf, Person, People, Book, Books} from './types'
-
+import Library from './books'
 
 // Utilities ------------------------------------
 function titlesToBooks(arr: string[]):Book[] {
@@ -20,85 +19,6 @@ function namesToPeople(arr: string[]):Person[] {
 }
 
 // Data -----------------------------------------
-const library:Books = {
-    "Mindset": {
-        title: "Mindset",
-        author: "Carol Dweck",
-        img: "https://images.gr-assets.com/books/1364251156l/1307582.jpg"
-    },
-    "Thinking in Systems": {
-        title: "Thinking in Systems",
-        author: "Donella Meadows",
-        img: "https://images-na.ssl-images-amazon.com/images/I/61npQHfWgAL.jpg",
-    },
-    "Why We Sleep: Unlocking the Power of Sleep and Dreams": {
-        title: "Why We Sleep: Unlocking the Power of Sleep and Dreams",
-        author: "Matthew Walker",
-        img: "https://images-na.ssl-images-amazon.com/images/I/81w6RZ6xm1L.jpg"
-    },
-    "The Hitchhiker's Guide to the Galaxy": {
-        title: "The Hitchhiker's Guide to the Galaxy",
-        author: "Douglas Adams",
-        img: "",
-    },
-    "Einstein: His Life and Universe": {
-        title: "Einstein: His Life and Universe",
-        author: "Walter Isaacson",
-        img: ""
-    },
-    "Structures: Or Why Things Don't Fall Down": {
-        title: "Structures: Or Why Things Don't Fall Down",
-        author: "J.E. Gordon",
-        img: "",
-    },
-    "Superintelligence: Paths, Dangers, Strategies": {
-        title: "Superintelligence: Paths, Dangers, Strategies",
-        author: "Nick Bostrom",
-        img: "",
-    },
-    "Zero to One: Notes on Startups, or How to Build the Future": {
-        title: "Zero to One: Notes on Startups, or How to Build the Future",
-        author: "Peter Thiel",
-        img: "",
-    },
-    "The Remains of the Day": {
-        title: "The Remains of the Day",
-        author: "Kazuo Ishiguro",
-        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/71d9G7xwaLL.jpg",
-    },
-    "Built to Last: Successful Habits of Visionary Companies": {
-        title: "Built to Last: Successful Habits of Visionary Companies",
-        author: "Jim Collins",
-        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/412OMhMA+lL._SX330_BO1,204,203,200_.jpg",
-    },
-    "Creation: Life and How to Make It": {
-        title: "Creation: Life and How to Make It",
-        author: "Steve Grand",
-        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/41Y5MRAJP9L._SX328_BO1,204,203,200_.jpg"
-    },
-    "Good to Great: Why Some Companies Make the Leap … and Others Don't": {
-        title: "Good to Great: Why Some Companies Make the Leap … and Others Don't",
-        author: "Jim Collins",
-        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/41VrxaCVU2L.jpg"
-    },
-    "The Innovator's Dilemma": {
-        title: "The Innovator's Dilemma",
-        author: "Clayton Christensen",
-        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2016/09/28/TB-3_The_Innovators_Dilemma.jpg",
-    },
-    "Sam Walton: Made in America": {
-        title: "Sam Walton: Made in America",
-        author: "Sam Walton",
-        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/51hdHY1gTSL._SX299_BO1,204,203,200_.jpg"
-    },
-    "The Mythical Man-Month": {
-        title: "The Mythical Man-Month",
-        author: "Frederick P. Brooks, Jr.",
-        img: "https://sc.cnbcfm.com/applications/cnbc.com/resources/files/2017/03/28/51XnDL5KC+L._SX334_BO1,204,203,200_.jpg"
-    }
-
-}
-
 const persons:People = {
     "Tobias Lütke" : {
         name: "Tobias Lütke",
@@ -126,6 +46,13 @@ const persons:People = {
         img: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/02/05/13/0.jpg?w968",
         description: "",
         company: "Facebook",
+        books: titlesToBooks([]),
+    },
+    "Bill Gates" : {
+        name: "Bill Gates",
+        img: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/02/05/13/0.jpg?w968",
+        description: "",
+        company: "Microsoft",
         books: titlesToBooks([]),
     }
 }
