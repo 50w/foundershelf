bafe147dda47e93d122536d055d25e440f83fa96
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-03-03 11:54:13 -0500

    global types file to import from

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface Book {
+    title: string;
+    author: string;
+    img: string;
+}
+
+export interface Books {
+    [key: string]: Book
+}
+
+export interface Person {
+    name: string;
+    img: string;
+    description: string;
+    company: string;
+    books: Book[];
+}
+
+export interface People {
+    [key: string]: Person
+}
+
+export interface Shelf {
+    people: Person[]
+}
