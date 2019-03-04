8c4e86bf821e22b97802bbcc2132fc7247738564
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-03-03 19:22:11 -0500

    Styling tweak

--- src/routes/Root/components/Founder/Founder.tsx
+++ src/routes/Root/components/Founder/Founder.tsx
@@ -32,12 +32,12 @@ const FullScreenDialog: React.SFC<any> = (props:Props) => {
             TransitionComponent={Transition}
         >       
             <div className='founder-content'>
-                <IconButton style={{position: 'absolute'}} color="inherit" onClick={handleClose} aria-label="Close">
+                <IconButton style={{position: 'absolute', zIndex:2}} color="inherit" onClick={handleClose} aria-label="Close">
                     <CloseIcon />
                 </IconButton>
                 <div className='founder-card'>
                     <div className='founder-header'>
-                        <Card style={{marginBottom: '0.25rem', padding: '0.5rem 1rem 0.5rem 1rem', textAlign: 'left', display: 'flex', flexDirection: 'row'}}>
+                        <Card raised style={{padding: '0.5rem 1rem 0.5rem 1rem', textAlign: 'left', display: 'flex', flexDirection: 'row'}}>
                             <img width={matches ? 200 : 100} height={matches ? 200 : 100} style={{margin: 'auto 1rem auto auto', borderRadius:'100%', objectFit: 'cover', border: '2px solid #021a40', boxShadow: '0px 8px 60px -10px rgba(13, 28, 39, 0.8)'}} src={person.img} />
                             <div>
                                 <h2 style={{marginBottom: 0}}>{person.name}</h2>
@@ -48,7 +48,6 @@ const FullScreenDialog: React.SFC<any> = (props:Props) => {
                             </div>
                         </Card>
                     </div> 
-
                 </div>
                 <BookGridList books={person.books}/>
             </div>
