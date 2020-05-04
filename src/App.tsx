7170d5ce1aa6b03ff9782b2eb5cb30b83266cb78
NickLewanowicz <nicklewanowicz@gmail.com>
2020-05-04 16:55:51 -0400

    Redo the whole header... wtf was I thinking

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Card from '@material-ui/core/Card';
+import Card from "@material-ui/core/Card";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import {Root} from './routes';
-import './App.css';
-import './mvp.css';
-
+import { Root } from "./routes";
+import "./App.css";
+import "./mvp.css";
 
 class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h2 className='header'>
-          FounderShelf.space
-          <a style={{color: 'white'}} target='_blank' href='https://www.github.com/nicklewanowicz/founder-shelfspace'><i className="fab fa-github"></i></a>
-        </h2>
-        <div className='element'/>
-        <div className="App">
-        <Card raised style={{margin: '1rem 0.25rem 1rem 0.25rem', padding: '0.5rem 1rem 0.5rem 1rem', textAlign: 'left'}}>
-            <div>
+      <div className="app">
+        <header>
+          <nav>
+            <h1>FounderShelf</h1>
+            <ul>
+              <li>
+                <a
+                  href="https://www.github.com/nicklewanowicz/founder-shelfspace"
+                  target="_blank"
+                >
+                  GitHub ↗
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <section>
+            <aside style={{width: 'auto'}}>
                 <h2>What is Founder Shelfspace?</h2>
+                <div style={{flexDirection: 'row'}}>
+
+                <img height={100} src="reading.svg"/>
                 <p>
-                  What does the bookshelf of the worlds most successful founders, entrepreneurs, and investors look like? Get context on how they think and browse the growing library of books they recommend...
+                  What does the bookshelf of the worlds most successful
+                  founders, entrepreneurs, and investors look like? Get context
+                  on how they think and browse the growing library of books they
+                  recommend.
                 </p>
-            </div>
-          </Card>
+              </div>
+            </aside>
+          </section>
+        </header>
+        <hr/>
+        <div className="App">
           <Router>
-            <Route path='/' component={Root}/>
+            <Route path="/" component={Root} />
           </Router>
         </div>
-        <div className='' style={{textAlign: 'center', margin: '1rem 0 2rem 0'}}>
-            Made with ❤️ by <a target='_blank' href='https://www.github.com/nicklewanowicz'>Nick</a> and <a target='_blank' href='https://www.github.com/foopert'>Connor</a> 
+        <div
+          className=""
+          style={{ textAlign: "center", margin: "1rem 0 2rem 0" }}
+        >
+          Made with ❤️ by{" "}
+          <a target="_blank" href="https://www.github.com/nicklewanowicz">
+            Nick
+          </a>{" "}
+          and{" "}
+          <a target="_blank" href="https://www.github.com/foopert">
+            Connor
+          </a>
         </div>
       </div>
     );
