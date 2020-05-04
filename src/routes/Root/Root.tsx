d1033c7f8fdaefdbea9d1fd91cdbfb07d05257e2
NickLewanowicz <nicklewanowicz@gmail.com>
2020-05-04 16:34:56 -0400

    Format and improve UX

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -1,39 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import {getRecommendationByFounder} from '../../content/data';
-import './Root.css'
-import { Person } from '../../content/types';
+import { getRecommendationByFounder } from "../../content/data";
+import "./Root.css";
+import { Person } from "../../content/types";
 
 export function Root() {
-  const [selected, setSelected] = useState<Person | undefined>()
-  const [detailsOpen, setDetailsOpen] = useState(false)
+  const [selected, setSelected] = useState<Person | undefined>();
+  const [detailsOpen, setDetailsOpen] = useState(false);
 
-  const founderClickOpen = (selected: Person)  => {
-    setSelected(selected)
-    setDetailsOpen(true)
+  const founderClickOpen = (selected: Person) => {
+    setSelected(selected);
+    setDetailsOpen(true);
   };
 
   const founderClose = () => {
-    setDetailsOpen(false)
+    setDetailsOpen(false);
   };
 
-  const founders = getRecommendationByFounder()
+  const founders = getRecommendationByFounder();
 
-  return (
-    <div className="persons">
-      {founderGrid(founders)}
-    </div>
-  );
+  return <div className="persons">{founderGrid(founders)}</div>;
 
   function founderGrid(founders: Person[]) {
-    return <section>{founders.map(founder => founderSquare(founder))}</section>
+    return (
+      <section>{founders.map((founder) => founderSquare(founder))}</section>
+    );
   }
 
-  function founderSquare({description, name, img}: Person) {
-  return <aside><img height={150} alt={name} src={img}/><h3>{name}</h3><p>{description}</p></aside>
+  function founderSquare({ books, company, description, name, img }: Person) {
+    const [open, setOpen] = useState(false)
+    console.log(open)
+    return (
+      <aside onClick={()=> setOpen(!open)}>
+        <img height={150} alt={name} src={img} />
+        <h2>{name}</h2>
+        <h3>{company}</h3>
+        <p>{`${description.substring(0,50)}...`}</p>
+      </aside>
+    );
   }
-
 }
 
 export default Root;
-
